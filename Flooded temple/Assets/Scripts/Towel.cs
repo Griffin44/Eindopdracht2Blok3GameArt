deac637028a991ec7826a07639bc0f05c91d8ecf
Griffin44 <Indogrff@hotmail.com>
2024-03-29 15:34:18 +0100

    Added water, lava and vfx

--- Flooded temple/Assets/Scripts/Towel.cs
+++ Flooded temple/Assets/Scripts/Towel.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class Towel : MonoBehaviour, IInteractable
 {
     public GameObject bathWater;
+    public GameObject bathLava;
+    public GameObject elemOrb;
 
     public void Interact() {
         Debug.Log("woosh");
         bathWater.GetComponent<Animator>().SetTrigger("BathDrain");
+        bathLava.GetComponent<Animator>().SetTrigger("BathDrain");
+        bathLava.GetComponent<Animator>().SetTrigger("BathDrain");
     }
 }
